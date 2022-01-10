a92d7e5987c8fb744a40961c5beab50ba45da000
Hritick Jaiswal <newbiehritick@gmail.com>
2022-01-10 10:43:03 +0530

    Fixed a bug

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
         <title>Spacetagram</title>
         <meta name="description" content="Instagram for Space Stuff" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
           rel="stylesheet"
