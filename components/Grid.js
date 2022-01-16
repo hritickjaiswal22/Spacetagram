1053bfe975c53ad3e8174b8b3b9d259f992a838c
Hritick Jaiswal <newbiehritick@gmail.com>
2022-01-16 18:50:53 +0530

    Added errorBox

--- components/Grid.js
+++ components/Grid.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { useDispatch } from "react-redux";
@@ -14,29 +14,38 @@ function Grid() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
+  const errorBoxRef = useRef();
 
   useEffect(() => {
     setIsLoading(true);
     axios
       .get(
-        "https://api.nasa.gov/planetary/apod?api_key=okjHDbbhtNkpPBaERuk0I0mmRWHyq1fbJRmOapAM&count=6"
+        "https://api.nasa.gov/planetary/apod?api_key=okjHDbbhtNkpPBaERuk0I0mmRWHyq1fbJRmOapAM&count=3"
       )
       .then((res) => {
         setIsLoading(false);
         setData(res.data);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        errorBoxRef.current.innerText =
+          error.message + " Please reload again!!";
+        errorBoxRef.current.classList.add(styles.active);
+      });
   }, []);
 
   function fetchMoreData() {
     axios
       .get(
-        "https://api.nasa.gov/planetary/apod?api_key=okjHDbbhtNkpPBaERuk0I0mmRWHyq1fbJRmOapAM&count=6"
+        "https://api.nasa.gov/planetary/apod?api_key=okjHDbbhtNkpPBaERuk0I0mmRWHyq1fbJRmOapAM&count=3"
       )
       .then((res) => {
         setData((prevData) => [...new Set([...prevData, ...res.data])]);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        errorBoxRef.current.innerText =
+          error.message + " Please reload again!!";
+        errorBoxRef.current.classList.add(styles.active);
+      });
   }
 
   function clickHandler(e) {
@@ -90,6 +99,7 @@ function Grid() {
         className={styles["my-masonry-grid"]}
         columnClassName={styles["my-masonry-grid_column"]}
       >
+        <aside ref={errorBoxRef} className={styles.errorBox}></aside>
         {data.map(({ date, media_type, url, title, explanation }) => {
           return (
             <Card
